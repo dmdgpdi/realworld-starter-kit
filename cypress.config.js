b9375c71955c40c36fec90c29bbf60c746da6e08
dmdgpdi <dmdgpdi@naver.com>
2024-03-21 18:25:55 +0900

    etc: cypress 종속성 추가

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};

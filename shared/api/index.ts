6d75d5dfdb70ae378ed3080fd6fbbb471fe49925
dmdgpdi <dmdgpdi@naver.com>
2024-04-02 16:02:58 +0900

    feat: BASE_URL 및 API 설정

---
+++ shared/api/index.ts
@@ -0,0 +1,16 @@
+const BASE_URL =
+  process.env.IS_MOCKING === 'enable'
+    ? 'http://localhost:9090'
+    : (process.env.NEXT_PUBLIC_BASE_URL as string);
+
+enum API {
+  ARTICLES = 'articles',
+  FEED = 'feed',
+  TAGS = 'tags',
+  PROFILES = 'profiles',
+  FOLLOW = 'follow',
+  USERS = 'users',
+  LOGIN = 'login',
+}
+
+export { BASE_URL, API };

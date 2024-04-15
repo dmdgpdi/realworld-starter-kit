2fff9c64ce3e30e25cef4a9590db5c428cfd3f8f
dmdgpdi <dmdgpdi@naver.com>
2024-04-15 15:27:54 +0900

    feat: 로그인 및 회원 가입 페이지 추가

---
+++ app/login/page.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+import { ContentPageLayout } from '@/shared/ui';
+import {
+  AuthContentLayout,
+  AuthH1,
+  AuthPageLayout,
+  AuthP,
+  authServerAction,
+} from '@/entities/auth';
+import { LoginForm } from '@/features/auth';
+
+export default async function LoginPage() {
+  const isLoggedIn = await authServerAction.hasToken();
+
+  if (isLoggedIn) {
+    redirect('/');
+  }
+
+  return (
+    <AuthPageLayout>
+      <ContentPageLayout>
+        <AuthContentLayout>
+          <AuthH1>Sign in</AuthH1>
+          <AuthP>
+            <Link href="/register">Need an account?</Link>
+          </AuthP>
+          <LoginForm />
+        </AuthContentLayout>
+      </ContentPageLayout>
+    </AuthPageLayout>
+  );
+}

e01680df320a9d0f75c90da631fa0314e74e47ec
dmdgpdi <dmdgpdi@naver.com>
2024-05-20 22:56:07 +0900

    refactor: ArticleList 컴포넌트 widgets 폴더로 이동

--- widgets/articleList/ArticleList.tsx
+++ widgets/articleList/ArticleList.tsx
@@ -1,41 +1,54 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
-import { articleLib, articleTypes } from '@/entities/article';
-import { ArticleDescriptionTag } from '@/entities/tag';
 import { isEmptyList } from '@/shared/lib';
+import {
+  articleLib,
+  articleTypes,
+  DateDescription,
+  ArticlePreviewLayout,
+  ArticleMetaLayout,
+  InforLayout,
+} from '@/entities/article';
+import { ArticleDescriptionTag } from '@/entities/tag';
+import { ToggleFavoriteButton } from '@/features/article';
 
 function ArticleItem({ article }: { article: articleTypes.ArticleType }) {
+  const { author, slug, favorited, favoritesCount } = article;
+
   return (
-    <article className="article-preview">
-      <div className="article-meta">
-        <Link href="/profile/eric-simons">
+    <ArticlePreviewLayout>
+      <ArticleMetaLayout>
+        <Link href={`/profile/${author.username}`}>
           <Image
-            src={article.author.image}
-            alt={article.author.username}
+            src={author.image}
+            alt={author.username}
             width={100}
             height={100}
           />
         </Link>
-        <div className="info">
-          <Link href={`/profile/${article.author.username}`} className="author">
-            {article.author.username}
+        <InforLayout>
+          <Link href={`/profile/${author.username}`} className="author">
+            {author.username}
           </Link>
-          <span className="date">
+          <DateDescription>
             {articleLib.formatDate(article.updatedAt)}
-          </span>
-        </div>
-        <button className="btn btn-outline-primary btn-sm pull-xs-right">
-          <i className="ion-heart"></i> {article.favorited}
-        </button>
-      </div>
+          </DateDescription>
+        </InforLayout>
+        <ToggleFavoriteButton
+          articleSlug={slug}
+          favorited={favorited}
+          favoritesCount={favoritesCount}
+          isSmall={true}
+        />
+      </ArticleMetaLayout>
       <Link href={`/article/${article.slug}`} className="preview-link">
         <h1>{article.title}</h1>
         <p>{article.description}</p>
         <span>Read more...</span>
         <ArticleDescriptionTag tagList={article.tagList} />
       </Link>
-    </article>
+    </ArticlePreviewLayout>
   );
 }
 

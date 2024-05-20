1efe25aa392b2a6e5290be48ef1c6dd2d1201d82
dmdgpdi <dmdgpdi@naver.com>
2024-05-20 22:43:51 +0900

    feat: article 조회 페이지 구현

---
+++ app/(article)/article/[articleSlug]/page.tsx
@@ -0,0 +1,65 @@
+import { redirect } from 'next/navigation';
+import { ContainerLayout, ContainerPageLayout, RowLayout } from '@/shared/ui';
+import {
+  ArticleBannerLayout,
+  ArticleContentLayout,
+  ArticlePageLayout,
+  articleApi,
+} from '@/entities/article';
+import { CommentLayout } from '@/entities/comment';
+import { ArticleMenu } from '@/widgets';
+import { CommentForm, CommentList } from '@/features/comment';
+import { ArticleDescriptionTag } from '@/entities/tag';
+
+export default async function ArticlePage({ params }: ArticlePageProps) {
+  let article;
+  const { articleSlug } = params;
+
+  try {
+    const { article: articleResponse } =
+      await articleApi.getArticle(articleSlug);
+    article = articleResponse;
+  } catch (e) {
+    redirect('/');
+  }
+
+  const { author } = article;
+  const { body, updatedAt, favorited, favoritesCount, tagList, slug } = article;
+
+  return (
+    <ArticlePageLayout>
+      <ArticleBannerLayout>
+        <ContainerLayout>
+          <h1>How to build webapps that scale</h1>
+
+          <ArticleMenu
+            author={author}
+            updatedAt={updatedAt}
+            favorited={favorited}
+            favoritesCount={favoritesCount}
+            articleSlug={slug}
+          />
+        </ContainerLayout>
+      </ArticleBannerLayout>
+
+      <ContainerPageLayout>
+        <ArticleContentLayout>
+          <p>{body}</p>
+          <ArticleDescriptionTag tagList={tagList} />
+        </ArticleContentLayout>
+        <hr />
+
+        <RowLayout>
+          <CommentLayout>
+            <CommentForm />
+            <CommentList articleSlug={articleSlug} />
+          </CommentLayout>
+        </RowLayout>
+      </ContainerPageLayout>
+    </ArticlePageLayout>
+  );
+}
+
+type ArticlePageProps = {
+  params: { articleSlug: string };
+};

e49a2eaf221f0dedd0cdca36d5f6aa38d4aaf266
dmdgpdi <dmdgpdi@naver.com>
2024-06-12 15:39:55 +0900

    test: cypress login command 추가

--- cypress/cypress.d.ts
+++ cypress/cypress.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
   interface Chainable {
     getBySel(value: string): Chainable<JQuery<HTMLElement>>;
+    login(email: string, password: string): void;
   }
 }
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,13 @@
 Cypress.Commands.add('getBySel', (selector, ...args) => {
   return cy.get(`[data-cy=${selector}]`, ...args);
 });
+
+Cypress.Commands.add('login', (email: string, password: string) => {
+  cy.visit('/login');
+  cy.intercept({ url: '/login', method: 'POST' }).as('login');
+  cy.getBySel('email-input').type(email);
+  cy.getBySel('password-input').type(password);
+  cy.getBySel('submit').click();
+  cy.wait('@login');
+  cy.visit('/');
+});

1b70f961d48dd7bf109f7b8b60fab2c45bbd8ef2
dmdgpdi <dmdgpdi@naver.com>
2024-06-12 17:26:24 +0900

    refactor: 사용하지 않는 auth 관련 라이브러리 함수 제거

--- entities/auth/lib/auth.lib.ts
+++ entities/auth/lib/auth.lib.ts
@@ -1,7 +1,4 @@
-import Cookies from 'js-cookie';
 import { AuthError } from '../auth.type';
-import { getLocalItemWithExpiry, setLocalItemWithExpiry } from '@/shared/lib';
-import { deleteLocalItem } from '@/shared/lib/localStorage';
 
 const parseAuthError = (error: {
   [key: string]: string[];
@@ -27,43 +24,4 @@ const parseAuthError = (error: {
   return parsedError;
 };
 
-const setClientAuthCookie = (data: string) => {
-  Cookies.set('auth', data, { expires: 1, sameSite: 'strict' });
-};
-
-const getClientAuthCookie = () => {
-  return Cookies.get('auth');
-};
-
-const getLocalStorageToken = () => {
-  const localToken = getLocalItemWithExpiry<{ token: string }>('token');
-
-  if (!localToken) {
-    return undefined;
-  }
-
-  return localToken.token;
-};
-
-const setLocalStorageToken = (token: string) => {
-  setLocalItemWithExpiry(
-    'token',
-    {
-      token: token,
-    },
-    { expiryInDay: 1 },
-  );
-};
-
-const deleteLocalStorageToken = () => {
-  deleteLocalItem('token');
-};
-
-export {
-  parseAuthError,
-  setClientAuthCookie,
-  getClientAuthCookie,
-  getLocalStorageToken,
-  setLocalStorageToken,
-  deleteLocalStorageToken,
-};
+export { parseAuthError };

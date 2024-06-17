73defe2e713ccfee31d3208753b2a1ec4acfd383
dmdgpdi <dmdgpdi@naver.com>
2024-06-17 16:05:28 +0900

    feat: user profile은 로그인하지 않아도 접근이 가능하게 수정

--- app/profile/[username]/layout.tsx
+++ app/profile/[username]/layout.tsx
@@ -1,19 +1,12 @@
 import { redirect } from 'next/navigation';
 import { decodeUrl } from '@/shared/lib';
 import { ProfilePageLayout, userApi } from '@/entities/user';
-import { authServerAction } from '@/entities/auth';
 import { ProfileInfo } from '@/widgets';
 
 export default async function ProfileLayout({
   params,
   children,
 }: ProfileLayoutProps) {
-  const token = await authServerAction.getAuthCookie();
-
-  if (!token) {
-    redirect('/login');
-  }
-
   const username = decodeUrl(params.username);
   let profile;
 

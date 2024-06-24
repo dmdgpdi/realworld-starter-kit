2ee27b77047612bcadc99c8f027720960fea8094
dmdgpdi <dmdgpdi@naver.com>
2024-06-24 17:28:26 +0900

    etc: 깃허브 액션을 위한 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const authToken = await authServerAction.getAuthCookie();
-
+  // something change for github action
   return (
     <html lang="en">
       <head></head>
